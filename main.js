8761343f6eb710704d8ade6af15df6808f17dbf1
Siddhartha Pathak <siddhartha.pathak0216@gmail.com>
2024-07-04 10:41:15 +0545

    HomeScreen Ready

--- main.js
+++ main.js
@@ -7,32 +7,31 @@ menuBtn.addEventListener("click", () => {
   navigation.classList.toggle("active");
 });
 
-
 // video navigation
 const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".video-slide");
 const contents = document.querySelectorAll(".content");
 
-var sliderNav = function(manual){
-    btns.forEach((btn) => {
-        btn.classList.remove("active");
-    });
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
 
-    contents.forEach((content) => {
-        content.classList.remove("active");
-    });
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
 
-    btns[manual].classList.add("active");
-    slides[manual].classList.add("active");
-    contents[manual].classList.add("active");
-}
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
 
 btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        sliderNav(i);
-    });
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
 });
